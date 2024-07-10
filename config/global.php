2738c04cff6b74119d6132f00d9c6f00e5f1f888
Alfred056 <alfredovilca374@gmail.com>
2024-07-10 00:04:03 -0500

    primer commit

---
+++ config/global.php
@@ -0,0 +1,20 @@
+<?php 
+// Ip de la pc servidor de base de datos
+define("DB_HOST", "mysql-miangel.alwaysdata.net");
+
+// Nombre de la base de datos
+define("DB_NAME", "miangel_bd");
+
+// Usuario de la base de datos
+define("DB_USERNAME","miangel_free2");
+
+// contraseña del usuario de la base de datos
+define("DB_PASSWORD", "W1rac0cha24");
+
+// definimos la codificación de los caracteres
+define("DB_ENCODE", "utf8");
+
+// Definimos la constante como nombre del proyecto
+define("PRO_NOMBRE", "ITVentas");
+
+?>
